59307c1dd8893cff4893daf02c2418cb7224c1b9
Yuya Ochiai <yuya0321@gmail.com>
2015-09-22 20:43:21 +0900

    ユーザー管理画面を修正

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,19 +1,28 @@
 'use strict';
 
 angular.module('feedScopeApp')
-  .controller('AdminCtrl', function($scope, $http, Auth, User) {
+  .controller('AdminCtrl', function($scope, $http, Auth, User, $mdDialog) {
 
     // Use the User $resource to fetch all users
     $scope.users = User.query();
 
-    $scope.delete = function(user) {
-      User.remove({
-        id: user._id
-      });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
+    $scope.delete = function(event, user) {
+      var procc = function(answer) {
+        console.log(answer);
+        User.remove({
+          id: user._id
+        });
+        angular.forEach($scope.users, function(u, i) {
+          if (u === user) {
+            $scope.users.splice(i, 1);
+          }
+        });
+      };
+      var confirm = $mdDialog.confirm()
+        .title('Would you like to delete ' + user.name + ' ?')
+        .targetEvent(event)
+        .ok('OK')
+        .cancel('Cancel');
+      $mdDialog.show(confirm).then(procc, function() {});
     };
   });
